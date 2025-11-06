0406f7ad56d41f60e098a1be255069085c265c4a
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-11-06 10:01:51 +0100

    initial commit

---
+++ src/app/(routes)/detalje/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const detalje = () => {
+    return ( 
+        <Link href="/">
+        <h1>Detalje Page</h1>
+        </Link>
+     );
+}
+ 
+export default detalje;
