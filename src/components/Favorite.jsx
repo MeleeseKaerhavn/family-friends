9d3bb72a0464cde09a4e3ba5161262b1949b60eb
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-11-10 12:59:55 +0100

    static page looks great

---
+++ src/components/Favorite.jsx
@@ -0,0 +1,10 @@
+import { SlStar } from "react-icons/sl";
+
+const Favorite = () => {
+    return (  <SlStar
+      className="col-start-1 row-start-1 justify-self-end m-2 z-10 text-white backdrop-blur-sm bg-white/20 border-none rounded-full p-1 shadow-md cursor-pointer"
+      size={24}
+      /> );
+}
+ 
+export default Favorite;
