449d9df84955be1ff1b876c370de00735a40b091
Meleese Kærhavn <meleesekaerhavn@gmail.com>
2025-11-12 10:50:12 +0100

    Dynamic and filters fine

--- src/components/AdoptButton.jsx
+++ src/components/AdoptButton.jsx
@@ -1,9 +1,12 @@
-const AdoptButton = () => {
-    return ( <div>
-    <button className="w-[90%] m-auto mb-4 bg-[#F2968F] text-white rounded-full px-6 py-3 flex justify-center items-center shadow-lg cursor-pointer">
-      <span className="font-semibold">Adoptér Samojed Willie</span>
-    </button>
-    </div> );
-}
- 
+
+const AdoptButton = ({ text = "Adopter" }) => {
+  return (
+    <div>
+      <button className="w-[90%] m-auto mb-4 bg-[#F2968F] text-white rounded-full px-6 py-3 flex justify-center items-center shadow-lg cursor-pointer">
+        <span className="font-semibold">Adopter {text}</span>
+      </button>
+    </div>
+  );
+};
+
 export default AdoptButton;
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,6 +1,16 @@
+// components/Tag.jsx
+const Tag = ({ label, color = "#ACD7FF", textColor = "#6B8B42" }) => {
+  return (
+    <p
+      className="rounded-full px-2 py-1 text-nowrap text-sm font-normal"
+      style={{
+        backgroundColor: color,
+        color: textColor,
+      }}
+    >
+      {label}
+    </p>
+  );
+};
 
-const Tag = () => {
-    return ( <p className="text-[#6B8B42] bg-[#ACD7FF] rounded-full px-2 py-1 text-nowrap"> Great Dane</p> );
-}
- 
 export default Tag;
